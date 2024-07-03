912138c2aa8ee76bd89eec9005a886a04e9e210a
andrecristen <andrecristen@users.noreply.github.com>
2024-07-03 16:29:03 +0000

    deploy: fc31c76aa468a9b1410c6ce4e2742c366209f13e

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: 'node',
-    verbose: true,
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    testMatch: ['**/src/**/*.test.js']
-  };
-  
